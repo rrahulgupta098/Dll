052584f40c13c806f20c8506238e7015c4fd7ca3
DELL <rrahulgupta098>
2024-07-31 20:36:02 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace Dll
+{
+    public class Class1
+    {
+        public int n1, n2, result;
+        public int pn1 
+      {
+            set { n1 = value; }
+        }
+        public int pn2
+        {
+            set { n2 = value; }
+        }
+
+        public int presult
+        {
+            get { return result; }
+        }
+
+        public void add()
+        {
+            result = n1 + n2;
+        }
+        public void sub()
+        {
+            result = n1 - n2;
+        }
+
+        public void mul()
+        {
+            result = n1*n2;
+        }
+
+        public void divide()
+        {
+            result = n1/n2;
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dll
+{
+    public class Class2
+    {
+        public int Square(int x)
+        {
+            return x * x;
+        }
+        public int cube(int x)
+        {
+            return Square(x)*x;
+        }
+
+    }
+}
